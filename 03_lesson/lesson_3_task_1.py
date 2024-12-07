d07c055c3cec6d7a3d249427e411b44df15d3fcb
Sky-PRO-Student-85-2 <c18867249@gmail.com>
2024-12-07 21:56:11 +0700

    03_lesson_python_Homework

---
+++ 03_lesson/lesson_3_task_1.py
@@ -0,0 +1,10 @@
+# Импорт класса User
+from user import User
+
+# Создание экземпляра класса
+my_user = User("Фродо", "Бэггинс")
+
+# Вызов методов класса
+my_user.sayname()
+my_user.saylastname()
+my_user.sayfullname()
